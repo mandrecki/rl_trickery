e8a049ea284754f08940bab87c9d81a0f8365d3e
Marian Andrecki <marian.andrecki+eqt@gmail.com>
2020-06-05 12:22:39 +0100

    strong performance a2c rnn on random mazes

--- rl_trickery/models/policy_networks.py
+++ rl_trickery/models/policy_networks.py
@@ -271,3 +271,197 @@ class MLPBase(NNBase):
         hidden_actor = self.actor(x)
 
         return self.critic_linear(hidden_critic), hidden_actor, rnn_hxs
+
+
+class NoTransition(nn.Module):
+    def __init__(self, hidden_size):
+        super(NoTransition, self).__init__()
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), nn.init.calculate_gain('relu'))
+        self.recurrent_hidden_state_size = 1
+        self.flat_lin = nn.Sequential(
+                Flatten(),
+                init_(nn.Linear(32 * 7 * 7, hidden_size)),
+                nn.ReLU()
+        )
+
+    def forward(self, x, rnn_state):
+        x = self.flat_lin(x)
+        return x, None
+
+
+class RNNTransition(nn.Module):
+    def __init__(self, hidden_size):
+        super(RNNTransition, self).__init__()
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0),
+                               nn.init.calculate_gain('relu'))
+        self.recurrent_hidden_state_size = 1
+        self.flat_lin = nn.Sequential(
+                Flatten(),
+                init_(nn.Linear(32 * 7 * 7, hidden_size)),
+                nn.ReLU()
+        )
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        for name, param in self.gru.named_parameters():
+            if 'bias' in name:
+                nn.init.constant_(param, 0)
+            elif 'weight' in name:
+                nn.init.orthogonal_(param)
+
+    def _forward_gru(self, x, hxs, masks):
+        if x.size(0) == hxs.size(0):
+            x, hxs = self.gru(x.unsqueeze(0), (hxs * masks).unsqueeze(0))
+            x = x.squeeze(0)
+            hxs = hxs.squeeze(0)
+        else:
+            # x is a (T, N, -1) tensor that has been flatten to (T * N, -1)
+            N = hxs.size(0)
+            T = int(x.size(0) / N)
+
+            # unflatten
+            x = x.view(T, N, x.size(1))
+
+            # Same deal with masks
+            masks = masks.view(T, N)
+
+            # Let's figure out which steps in the sequence have a zero for any agent
+            # We will always assume t=0 has a zero in it as that makes the logic cleaner
+            has_zeros = ((masks[1:] == 0.0) \
+                            .any(dim=-1)
+                            .nonzero()
+                            .squeeze()
+                            .cpu())
+
+            # +1 to correct the masks[1:]
+            if has_zeros.dim() == 0:
+                # Deal with scalar
+                has_zeros = [has_zeros.item() + 1]
+            else:
+                has_zeros = (has_zeros + 1).numpy().tolist()
+
+            # add t=0 and t=T to the list
+            has_zeros = [0] + has_zeros + [T]
+
+            hxs = hxs.unsqueeze(0)
+            outputs = []
+            for i in range(len(has_zeros) - 1):
+                # We can now process steps that don't have any zeros in masks together!
+                # This is much faster
+                start_idx = has_zeros[i]
+                end_idx = has_zeros[i + 1]
+
+                rnn_scores, hxs = self.gru(
+                    x[start_idx:end_idx],
+                    hxs * masks[start_idx].view(1, -1, 1))
+
+                outputs.append(rnn_scores)
+
+            # assert len(outputs) == T
+            # x is a (T, N, -1) tensor
+            x = torch.cat(outputs, dim=0)
+            # flatten
+            x = x.view(T * N, -1)
+            hxs = hxs.squeeze(0)
+
+        return x, hxs
+
+    def forward(self, x, rnn_state):
+        x = self.flat_lin(x)
+        x, rnn_state = self.rnn(x, rnn_state)
+
+        return x, rnn_state
+
+
+class RecurrentPolicy(nn.Module):
+    def __init__(self, obs_shape, action_space, architecture, hidden_size):
+        super(RecurrentPolicy, self).__init__()
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), nn.init.calculate_gain('relu'))
+
+        num_inputs = obs_shape[0]
+        im_size = obs_shape[1]
+        assert im_size == obs_shape[2]
+        self.spatial_latent_size = (7, 7)
+        self.hidden_size = hidden_size
+        self.architecture = architecture
+        self.is_recurrent = architecture in ["rnn", "crnn"]
+
+        if im_size == 84:
+            self.encoder = nn.Sequential(
+                init_(nn.Conv2d(num_inputs, 32, 8, stride=4)), nn.ReLU(),
+                init_(nn.Conv2d(32, 64, 4, stride=2)), nn.ReLU(),
+                init_(nn.Conv2d(64, 32, 3, stride=1)), nn.ReLU(),
+            )
+        elif im_size == 64:
+            self.encoder = nn.Sequential(
+                # input (x, 64, 64)
+                init_(nn.Conv2d(num_inputs, 32, 6, stride=4, padding=1)), nn.ReLU(),
+                # input (3, 16, 16)
+                init_(nn.Conv2d(32, 64, 4, stride=2, padding=2)), nn.ReLU(),
+                # input (3, 9, 9)
+                init_(nn.Conv2d(64, 32, 3, stride=1)), nn.ReLU(),
+                # input (3, 7, 7)
+            )
+        else:
+            raise NotImplementedError
+
+        if architecture == "ff":
+            self.transition = NoTransition(hidden_size)
+            self.recurrent_hidden_state_size = self.transition.recurrent_hidden_state_size
+            self.recurse = nn.Sequential(
+                Flatten(),
+                init_(nn.Linear(32 * 7 * 7, hidden_size)),
+                nn.ReLU()
+            )
+        elif architecture == "rnn":
+
+            self.recurrent_hidden_state_size = hidden_size
+            pass
+        elif architecture == "crnn":
+            self.recurrent_hidden_state_size = None
+            pass
+        else:
+            raise NotImplementedError
+
+
+        if action_space.__class__.__name__ == "Discrete":
+            num_outputs = action_space.n
+            self.dist = Categorical(self.hidden_size, num_outputs)
+        elif action_space.__class__.__name__ == "Box":
+            num_outputs = action_space.shape[0]
+            self.dist = DiagGaussian(self.hidden_size, num_outputs)
+        elif action_space.__class__.__name__ == "MultiBinary":
+            num_outputs = action_space.shape[0]
+            self.dist = Bernoulli(self.hidden_size, num_outputs)
+        else:
+            raise NotImplementedError
+
+
+    def forward(self, inputs, rnn_hxs, masks):
+        raise NotImplementedError
+
+    def act(self, inputs, rnn_hxs, masks, deterministic=False):
+        value, actor_features, rnn_hxs = self.base(inputs, rnn_hxs, masks)
+        dist = self.dist(actor_features)
+
+        if deterministic:
+            action = dist.mode()
+        else:
+            action = dist.sample()
+
+        action_log_probs = dist.log_probs(action)
+        dist_entropy = dist.entropy().mean()
+
+        return value, action, action_log_probs, rnn_hxs
+
+    def get_value(self, inputs, rnn_hxs, masks):
+        value, _, _ = self.base(inputs, rnn_hxs, masks)
+        return value
+
+    def evaluate_actions(self, inputs, rnn_hxs, masks, action):
+        value, actor_features, rnn_hxs = self.base(inputs, rnn_hxs, masks)
+        dist = self.dist(actor_features)
+
+        action_log_probs = dist.log_probs(action)
+        dist_entropy = dist.entropy().mean()
+
+        return value, action_log_probs, dist_entropy, rnn_hxs
