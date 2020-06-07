7a2f9ce6839cd1526db838e4516db1d8bc467bbf
Marian Andrecki <marian.andrecki+eqt@gmail.com>
2020-06-07 12:15:51 +0100

    run analysis added

--- multirun.sh
+++ multirun.sh
@@ -1,6 +1,1 @@
-python train.py agent=a2c
-python train.py agent=a2c_rnn
-python train.py agent=a2c_3rnn
-python train.py agent=a2c_crnn
-python train.py agent=a2c_3crnn
-
+python train.py -m agent=a2c,a2c_rnn,a2c_3rnn,a2c_crnn,a2c_3crnn,ppo,ppo_3crnn,ppo_3rnn,ppo_crnn,ppo_rnn 
